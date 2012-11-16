c5db3848bfc8426e2fe5b35cd7ad205a25112b5b
Siyuan Ma <siyuan.ma9@gmail.com>
2012-11-15 20:29:34 -0500

    Update tests/TraceReplayerBatchTest.sh

--- tests/TraceReplayerBatchTest.sh
+++ tests/TraceReplayerBatchTest.sh
@@ -4,12 +4,13 @@ TAG=$1;
 PERIOD=$2; #Seconds
 DPATH=$3;
 TPATH=$4;
+ISBLK=$5;
 
 JVM_ARGS="-Xmx2048m -XX:MaxPermSize=256m"
 
 if [ $# -lt 4 ]
 then
-	echo "CMD TAG PERIOD DPATH TPATH";
+	echo "CMD TAG PERIOD DPATH TPATH ISBLK";
     exit;
 fi
 
@@ -21,6 +22,7 @@ LABEL=TraceReplayerBatch;
 python ${ROOT}/tool/ioscm-conf.py -f $CONFIG -k period -v $PERIOD -l $LABEL;
 python ${ROOT}/tool/ioscm-conf.py -f $CONFIG -k dataDir -v "${DPATH}/" -l $LABEL;
 python ${ROOT}/tool/ioscm-conf.py -f $CONFIG -k traceDir -v "${TPATH}/" -l $LABEL;
+python ${ROOT}/tool/ioscm-conf.py -f $CONFIG -k isBlock -v "${ISBLK}" -l $LABEL;
 
 java $JVM_ARGS -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Log4JLogger \
 	-Dlog4j.configuration=${ROOT}/conf/log4j.properties -classpath ${ROOT}/dist/lib/${JAR} org.apache.ioscm.CongestionMeter -conf ${CONFIG} -tag ${TAG}
